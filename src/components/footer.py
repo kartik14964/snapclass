8959ce4cc1e1fe32d24f9cf343b62a153ada18d5
Kartik Rawat <kartikrawat14964@gmail.com>
2026-09-06 03:34:11 +0530

    streamlit setup homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+            <p style="font-weight:bold; color:white;"> Created with ❤️ by kartik </p>  
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+            <p style="font-weight:bold; color:black;"> Created with ❤️ by kartik </p>  
+        </div>
+    """, unsafe_allow_html=True)
