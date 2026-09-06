e370cbd53c1bda1b713c3efd29c034fea4e0db6b
Kartik Rawat <kartikrawat14964@gmail.com>
2026-09-06 21:24:04 +0530

    Fix back button QR code bug and UI crashes

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -22,9 +22,10 @@ def student_dashboard():
         header_dashboard()
     with c2:
         st.subheader(f"""Welcome, {student_data['name']} """)
-        if st.button("Logout", type='secondary', key='loginbackbtn', shortcut="control+backspace"):
+        if st.button("Logout", type='secondary', key='student_logout_btn'):
             st.session_state['is_logged_in'] = False
             del st.session_state.student_data 
+            st.query_params.clear()
             st.rerun()
 
 
@@ -66,10 +67,10 @@ def student_dashboard():
 
 
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
-        def unenroll_button():
-                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
-                    unenroll_student_to_subject(student_id, sid)
-                    st.toast(f'Unenrolled from {sub['name']} successfully!')
+        def unenroll_button(current_sid=sid, current_name=sub['name']):
+                if st.button("Unenroll from this course", type='tertiary', use_container_width=True, icon=':material/delete_forever:', key=f"unenroll_{current_sid}"):
+                    unenroll_student_to_subject(student_id, current_sid)
+                    st.toast(f"Unenrolled from {current_name} successfully!")
                     st.rerun()
 
         with cols[i % 2]:
@@ -102,8 +103,9 @@ def student_screen():
     with c1:
         header_dashboard()
     with c2:
-        if st.button("Go back to Home", type='secondary', key='loginbackbtn', shortcut="control+backspace"):
+        if st.button("Go back to Home", type='secondary', key='student_home_btn'):
             st.session_state['login_type'] = None
+            st.query_params.clear()
             st.rerun()
 
     st.header('Login using FaceID', text_alignment='center')
@@ -133,7 +135,7 @@ def student_screen():
                         st.session_state.is_logged_in = True
                         st.session_state.user_role = 'student'
                         st.session_state.student_data = student
-                        st.toast(f'Welcome Back {student['name']}')
+                        st.toast(f"Welcome Back {student['name']}")
                         time.sleep(1)
                         st.rerun()
                 else:
